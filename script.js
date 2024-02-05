88ba364039de1319287dfd7f871cddfcd04d8eaa
myradio <154856702+pristaaaa@users.noreply.github.com>
2024-02-05 08:11:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ jwplayer("video").setup({
   height: "100%",
   autostart: true,
   mute: false,
-  file: "https://prod-sports-south-gm.jiocinema.com/hls/live/2099915/hd_akamai_merged_avc_hin_sa20_m1_040224/master.m3u8"
+  file: "https://prod-sports-hin-gm.jiocinema.com/hls/live/2100322/hd_akamai_iosmob_avc_hin_indvseng_test02day04050224/master.m3u8"
 });

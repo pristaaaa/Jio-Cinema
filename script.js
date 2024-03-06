679639f0ee20039563a0b723f64fc2bb9a3fa2ef
myradio <154856702+pristaaaa@users.noreply.github.com>
2024-03-06 18:28:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ jwplayer("video").setup({
   height: "100%",
   autostart: true,
   mute: false,
-  file: "https://prod-ent-live-gm.jiocinema.com/hls/live/2100322/uhd_akamai_ctv_avc_hin_wpl_s1_m1050324/master.m3u8"
+  file: "https://prod-ent-live-gm.jiocinema.com/hls/live/2100322/uhd_akamai_ctv_avc_hin_wpl_s1_m1060324/master.m3u8"
 });
